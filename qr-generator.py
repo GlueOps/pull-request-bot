8b86cd8f9fd66f4c9d74a0596684dfefaf282cd1
Mbaoma <mmaryraphaella@gmail.com>
2023-06-13 16:35:55 +0100

    add code to generate QR code, without relying on 3rd party dependency

--- qr-generator.py
+++ qr-generator.py
@@ -3,6 +3,6 @@ import qrcode
 def generate_qr_code(url):
     img = qrcode.make(url)
     type(img)
-    img.save("Pull-Request-URL.png") #save the image with the name img.png
+    img.save("Pull-Request-URL.png") #save the image with the name "Pull-Request-URL.png"
 url = input("Enter the url: ")
 generate_qr_code(url)
