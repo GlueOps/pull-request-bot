55e38941cd3c15452a013e59f1a1241b856b92a7
Mary Mbaoma <mmaryraphaella@gmail.com>
2023-07-04 22:02:40 +0000

    edit requirements file

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ import time
 import requests
 from kubernetes import client, config
 
-from src.get_github_api_token import get_github_api_token
-from src.json_log_formatter import JsonFormatter
-
-import qrcode
-from io import BytesIO
-import base64
-
-buffer = BytesIO()
-
 #=== configure logging
 # json formatter
 json_formatter = JsonFormatter()
@@ -214,7 +205,7 @@ def get_comment(git_commit_metadata, app_name, app_argocd_url, external_urls, ap
     body += get_first_column("🖥️", "Deployment Preview") + '[' + external_urls[0] + '](' + external_urls[0] + ') |'
     body += get_first_column("📊", "Metrics") + '[Grafana](' + app_metrics_url + ') |'
     body += get_first_column("📜", "Logs") + '[Loki](' + app_logs_url + ') |'
-    qr_code_url = f'https://mbaoma-ominous-yodel-7pj9wxg69jx2w6j5-8000.preview.app.github.dev/v1/qr?url={external_urls[0]}'
+    qr_code_url = f'https://127.0.0.1:8000/v1/qr?url={external_urls[0]}'
     body += get_first_column("📱", "Preview on mobile") + f'<img src="{qr_code_url}" width="150" height="150">|'
 
     return body
